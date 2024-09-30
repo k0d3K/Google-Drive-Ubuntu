cdca51328cbbd89a5fb2c60034b62a78bdc63fa3
Lucien Guerbigny <153361954+k0d3K@users.noreply.github.com>
2024-09-30 18:31:06 +0200

    Update google_drive_monitor.sh
    adding a time cooldown

--- google_drive_monitor.sh
+++ google_drive_monitor.sh
@@ -1,14 +1,37 @@
 #!/bin/bash
 
 # Replace with the full path to the file you want to monitor
-file_to_monitor="/home/lucien/Documents"
+file_to_monitor="/home/lucien/Documents/Google Drive"
 
 # Replace with the rclone command you want to execute
-rclone_command="rclone sync 'Google Drive': '/home/lucien/Documents/Google Drive'"
+rclone_command="rclone sync '/home/lucien/Documents/Google Drive' 'Google Drive':"
+
+# Set the cooldown period in seconds (1 hour in this case)
+cooldown_period=3600
+
+# Variable to store the last execution time
+last_execution_time=0
+
+# Function to check if the cooldown period has elapsed
+function cooldown_elapsed() {
+    current_time=$(date +%s)
+    if (( current_time - last_execution_time >= cooldown_period )); then
+        return 0  # Cooldown has elapsed
+    else
+        return 1  # Cooldown has not elapsed
+    fi
+}
 
 # Use inotifywait to monitor the folder for changes
-inotifywait -m -e modify,create,delete "$folder_to_monitor" | while read -r event file; do
-  echo "Event: $event, File: $file"
-  # Execute your rclone command here
-  $rclone_command
+inotifywait -m -r -e modify,create,delete,moved_to,moved_from "$folder_to_monitor" | while read -r event file; do
+    echo "Event: $event, File: $file"
+
+    # Check if the cooldown period has elapsed
+    if cooldown_elapsed; then
+        echo "Running rclone command: $rclone_command"
+        $rclone_command
+        last_execution_time=$(date +%s)
+    else
+        echo "Cooldown period not elapsed. Skipping rclone command."
+    fi
 done
